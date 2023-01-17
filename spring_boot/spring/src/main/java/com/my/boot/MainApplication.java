01206a64c2c1e980a58f9b7f83da8a89d2eb1e70
unknown <mayang379051737@163.com>
2023-01-17 17:34:57 +0800

    change

--- spring_boot/spring/src/main/java/com/my/boot/MainApplication.java
+++ spring_boot/spring/src/main/java/com/my/boot/MainApplication.java
@@ -1,10 +1,13 @@
 package com.my.boot;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.my.boot.dao")
 public class MainApplication {
     public static void main(String[] args_) throws Exception {
-        return ;
+        SpringApplication.run(MainApplication.class, args_);
     }
 }
--- spring_boot/spring/src/main/java/com/my/boot/aop/AroundCut.java
+++ spring_boot/spring/src/main/java/com/my/boot/aop/AroundCut.java
@@ -17,17 +17,4 @@ import javax.servlet.http.HttpSession;
 public class AroundCut {
     public static final String POINT_CUT = "execution(* com.my.boot.controller.AuthorityController.*(..)) || " +
             "execution(* com.my.boot.controller.UserController.*(..)) || ";
-
-
-
-    @Around(AroundCut.POINT_CUT)
-    public DataResult checkSession(ProceedingJoinPoint pjp) throws Throwable {
-        //获取session
-        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession();
-        Object userInfo = session.getAttribute("userInfo");
-        if(null == userInfo){//session不存在, 返回登录失效
-            return DataResult.errByErrCode(Code.LOGIN_OUT);
-        }
-        return (DataResult) pjp.proceed();
-    }
 }
